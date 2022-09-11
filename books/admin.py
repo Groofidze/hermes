6c20f22fe2f506ed964a48895d6869bdd67b0880
Groofi <groofi@list.ru>
2022-09-11 19:04:40 +0300

    edit admin

--- books/admin.py
+++ books/admin.py
@@ -4,16 +4,17 @@ from books.models import Books, Photos
 
 @admin.register(Books)
 class BooksAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    fields = ['author', 'name', 'management', 'made_in', 'year']
-    ordering = ['name']
+    list_display_links = ('name',)
+    list_display = ('name', 'author',)
+    fields = ('author', 'name', 'made_in', 'year', 'management',)
+    ordering = ('name',)
 
 
 @admin.register(Photos)
 class PhotosAdmin(admin.ModelAdmin):
     list_display = ('book',)
-    fields = ['book', 'photo']
-    ordering = ['book']
+    fields = ('book', 'photo',)
+    ordering = ('book',)
 
     def get_queryset(self, request):
         return Photos.objects.select_related("book").all()
